4bddb2e1ced5cfe2fda3e6ecdaf8bcaff947cc82
korelator <honza-valenta@seznam.cz>
2017-05-20 12:46:00 +0200

    Added options class

--- src/minesweeper/players/HumanAscii.java
+++ src/minesweeper/players/HumanAscii.java
@@ -6,6 +6,7 @@
 package minesweeper.players;
 
 import java.util.Scanner;
+import minesweeper.boards.IBoard;
 
 /**
  * player playing on ascii table with Scanner IO
@@ -14,25 +15,54 @@ import java.util.Scanner;
 public class HumanAscii implements IPlayer 
 {
     protected Scanner sc;
+    private IBoard board;
     
     public HumanAscii ()
     {
         sc = new Scanner(System.in, "Windows-1250");
     }
     
-    
-    
     @Override
-    public Click playTurn() 
+    public Click playTurn()
     {
-        
-        return null;
+        int mX = enterByScanner("Zadejte pozici X: ");
+        int mY = enterByScanner("Zadejte pozici Y: ");
+        return new Click(mX, mY, true);
     }
 
+    private int enterByScanner(String message)
+    {
+        int result = -1;
+        boolean succesful = false;
+        String s = "";
+        do
+        {
+            try
+            {
+                System.out.println(message);
+                s = sc.nextLine();
+                result = Integer.parseInt(s);
+                succesful = true;
+            }
+            catch (NumberFormatException vyjimka)
+            {
+                System.out.println(s + " je neplatný vstup, zkus to znovu");
+            }
+        }
+        while(!succesful);
+        return result;
+    }
+    
     @Override
     public void happened()
     {
-        
+        board.show();
+    }
+
+    @Override
+    public void setBoard(IBoard tableForPlayer)
+    {
+        board = tableForPlayer;
     }
     
 }
